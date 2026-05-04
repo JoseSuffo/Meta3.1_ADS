12a2f30bbe846dd4d7b676073282255f9e14c6bd
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2026-05-03 20:37:50 -0700

    Implementación del componente Panel LED para avisar de bebidas y platos listos.

--- src/tabernaComponentes/PanelLED.java
+++ src/tabernaComponentes/PanelLED.java
@@ -1,4 +1,17 @@
 package tabernaComponentes;
 
-public class PanelLED {
+import tabernaBus.Evento;
+import tabernaBus.Suscriptor;
+import tabernaEventos.BebidaServidaEvent;
+import tabernaEventos.ComidaPreparadaEvent;
+
+public class PanelLED implements Suscriptor<Evento> {
+    @Override
+    public void onEvent(Evento evento) {
+        if (evento instanceof BebidaServidaEvent e) {
+            System.out.println("[PANEL LED] ¡Nueva bebida servida en la Mesa " + e.mesaId() + " (" + e.bebida() + ")!");
+        } else if (evento instanceof ComidaPreparadaEvent e) {
+            System.out.println("[PANEL LED] ¡Plato listo: " + e.plato() + " para el pedido " + e.pedidoId() + "!");
+        }
+    }
 }
