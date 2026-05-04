854106d5ae0efd370b8730ec05d640ae77846236
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2026-05-03 21:50:40 -0700

    Implementación del componente Sistema de Humo para activarse solo bajo la canción definida, demostrando extensibilidad.

--- src/tabernaComponentes/SistemaDeHumo.java
+++ src/tabernaComponentes/SistemaDeHumo.java
@@ -1,4 +1,17 @@
 package tabernaComponentes;
 
-public class SistemaDeHumo {
+import tabernaBus.Suscriptor;
+import tabernaEventos.BandaTocandoEvent;
+
+public class SistemaDeHumo implements Suscriptor<BandaTocandoEvent> {
+
+    @Override
+    public void onEvent(BandaTocandoEvent evento) {
+        if (evento.nombreCancion().equalsIgnoreCase("Through the Fire and Flames")) {
+            System.out.println("\n[SISTEMA DE HUMO] ¡ATENCIÓN! Se detectó una canción legendaria.");
+            System.out.println("[SISTEMA DE HUMO] DISPARANDO RÁFAGAS DE HUMO Y FUEGO... ¡ESTO ES ÉPICO! \n");
+        } else {
+            System.out.println("[SISTEMA DE HUMO] (En espera... esta canción no es lo suficientemente épica)");
+        }
+    }
 }
