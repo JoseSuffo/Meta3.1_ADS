6430ff9e45e0c3d7e6488efa5135c726a1d6fec7
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2026-05-03 21:30:29 -0700

    Implementación del componente Sistema de Sonido para activar las canciones del flujo general del programa.

--- src/tabernaComponentes/SistemaDeSonido.java
+++ src/tabernaComponentes/SistemaDeSonido.java
@@ -1,4 +1,11 @@
 package tabernaComponentes;
 
-public class SistemaDeSonido {
+import tabernaBus.Suscriptor;
+import tabernaEventos.BandaTocandoEvent;
+
+public class SistemaDeSonido implements Suscriptor<BandaTocandoEvent> {
+    @Override
+    public void onEvent(BandaTocandoEvent evento) {
+        System.out.println("[SONIDO] Ajustando ecualizador para: [" + evento.nombreCancion() + "] de " + evento.nombreBanda());
+    }
 }
