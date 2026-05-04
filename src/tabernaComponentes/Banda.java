0aafaf4d94a51e4b4d85563c53b75211265fe182
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2026-05-03 21:44:44 -0700

    Implementación del suscriptor Banda para la ejecución de canciones de una banda en el flujo principal del programa.

--- src/tabernaComponentes/Banda.java
+++ src/tabernaComponentes/Banda.java
@@ -1,4 +1,17 @@
 package tabernaComponentes;
 
+import tabernaBus.EventBus;
+import tabernaEventos.BandaTocandoEvent;
+
 public class Banda {
+    private final EventBus bus;
+
+    public Banda(EventBus bus) {
+        this.bus = bus;
+    }
+
+    public void tocarCancion(String banda, String cancion, int duracion) {
+        System.out.println("[BANDA] ¡Empieza una nueva canción!");
+        bus.publicar(new BandaTocandoEvent(banda, cancion, duracion));
+    }
 }
